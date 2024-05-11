a1aa4c0dcf0227873a679db7227cd361353b843d
mishagodovanuk <mishagodovanuk@gmail.com>
2024-05-11 16:45:52 +0300

    add base functionality

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+    ];
+
+    protected $casts = [
+        'content' => 'binary',
+    ];
+
+    protected $hidden = [
+        'user_id',
+    ];
+}
