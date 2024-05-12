28e875a5596a60dda39b6a4d9c6e17a078fa30f5
mishagodovanuk <mishagodovanuk@gmail.com>
2024-05-12 13:31:24 +0300

    add login/register functionality

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * Class HomeController
+ */
 class HomeController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    public function execute()
+    /**
+     * @return Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+     */
+    public function execute(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
         return view('frontend.pages.home');
     }
